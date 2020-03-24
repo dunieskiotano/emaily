465da458f32c7f82b816c48be9001df2af9f57de
Dunieski Otano <dunieskior@gmail.com>
2020-03-24 17:37:31 -0400

    changed text

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  //res.send({ hi: 'there' });
+  res.send('Te amo, mami linda')
 });
 
 const PORT = process.env.PORT || 5000;
